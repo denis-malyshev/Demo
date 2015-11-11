5adc8f53ec4b2e9caebd3e22d7e4be9ddba62efd
Denis Malyshev <denis.malyshev@teamdev.com>
2015-11-11 14:49:16 +0200

    Refactoring XmlParser.

--- Main/src/com/teamdev/demo/XmlParser.java
+++ Main/src/com/teamdev/demo/XmlParser.java
@@ -7,7 +7,7 @@ import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.ArrayList;
 
-public class CategoryParser {
+public class XmlParser {
 
     public static ArrayList<Category> getCategories(String fileName) {
         JAXBContext context;
