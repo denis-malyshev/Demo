54e371e68348ff1db0341b2e9769deb82f21f18e
Denis Malyshev <denis.malyshev@teamdev.com>
2015-11-11 17:13:39 +0200

    Refactoring DemoFrame.

--- Main/src/com/teamdev/demo/DemoFrame.java
+++ Main/src/com/teamdev/demo/DemoFrame.java
@@ -10,56 +10,35 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public final class DemoFrame {
-    private final JFrame frame;
-    private final JPanel leftPanel = new JPanel();
-    private final JPanel rightContainer = new JPanel();
-    private final JPanel rightDownSubContainer = new JPanel();
+    private JFrame frame;
+    private JPanel leftPanel;
+    private JPanel rightContainer;
+    private JPanel rightDownSubContainer;
     private final JLabel labelAboutExample = new JLabel("Click on example for showing");
-    private final JSplitPane mainContainer = new JSplitPane();
-    private final JTabbedPane tabbedPane = new JTabbedPane();
-    private final JScrollPane leftScrollPane = new JScrollPane(leftPanel);
-    private final JPanel preview = new JPanel();
-    private final JTextPane source = new JTextPane();
-    private final JScrollPane sourceContainer = new JScrollPane(source);
+    private JSplitPane mainContainer;
+    private JTabbedPane tabbedPane;
+    private JScrollPane leftContainer;
+    private JPanel preview;
+    private JTextPane source;
+    private JScrollPane sourceContainer;
     private final JTree treeOfExample;
     private final SampleProvider sampleProvider;
 
     public DemoFrame() {
+        initializeLeftContainer();
+        initializeTabbedPane();
+        initializeRightContainer();
+        initializeMainContainer();
+        initializeDemoFrame();
+
         sampleProvider = new SampleProvider(preview);
         final JavaHighlighter javaHighlighter = new JavaHighlighter(source, sourceContainer);
-        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-        frame = new JFrame();
-        frame.setSize(800, 500);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLocation((dimension.width - frame.getWidth()) / 2, (dimension.height - frame.getHeight()) / 2);
-        frame.setTitle("Demo");
-
-        leftPanel.setMinimumSize(new Dimension(100, frame.getHeight()));
-        leftPanel.setBackground(Color.white);
-
-        rightContainer.setLayout(new BorderLayout());
-        rightContainer.add(labelAboutExample,BorderLayout.NORTH);
-        rightContainer.add(rightDownSubContainer,BorderLayout.CENTER);
-
-        rightDownSubContainer.setLayout(new GridLayout());
-        rightDownSubContainer.add(tabbedPane);
-
-        preview.setLayout(new BorderLayout());
-        tabbedPane.addTab("Preview", preview);
-        tabbedPane.addTab("Source", sourceContainer);
-        tabbedPane.setVisible(false);
-
-        mainContainer.setLeftComponent(leftScrollPane);
-        mainContainer.setRightComponent(rightContainer);
-
-        frame.add(mainContainer);
 
         final ArrayList<Category> categories = XmlParser.getCategories("data.xml");
         treeOfExample = new JTree(DataProvider.createRootTreeNode(categories));
         treeOfExample.setRootVisible(false);
         treeOfExample.expandRow(0);
-        leftPanel.add(treeOfExample);//tabbedPane.setVisible(true);
+        leftPanel.add(treeOfExample);
         treeOfExample.addTreeSelectionListener(new TreeSelectionListener() {
             @Override
             public void valueChanged(TreeSelectionEvent e) {
@@ -67,11 +46,9 @@ public final class DemoFrame {
                 for (Category category : categories) {
                     for (SampleInfo sampleInfo : category.getSampleInfo()) {
                         if (sampleInfo.getName().toString() == treePath.getLastPathComponent().toString()) {
-                            preview.removeAll();
-                            preview.revalidate();
-                            tabbedPane.setVisible(true);
-                            tabbedPane.setSelectedIndex(0);
-                            labelAboutExample.setText("<html>"+sampleInfo.getDescription()+"</html>");
+                            updatePreview();
+                            updateTabbedPane();
+                            labelAboutExample.setText("<html>" + sampleInfo.getDescription() + "</html>");
                             sampleProvider.invokeInstance(sampleInfo.getName());
                             setSourceText(DataProvider.getSourceCode(sampleInfo.getName()));
                             javaHighlighter.highlightCode();
@@ -85,6 +62,64 @@ public final class DemoFrame {
     private void setSourceText(String text) {
         source.setText(text);
         source.getDocument().putProperty(DefaultEditorKit.EndOfLineStringProperty, "\n");
+    }
+
+    private void initializeDemoFrame() {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        frame = new JFrame();
+        frame.setSize(800, 500);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocation((dimension.width - frame.getWidth()) / 2, (dimension.height - frame.getHeight()) / 2);
+        frame.setTitle("Demo");
+        frame.add(mainContainer);
+    }
+
+    private void initializeLeftContainer() {
+        leftPanel = new JPanel();
+        leftContainer = new JScrollPane(leftPanel);
+        leftPanel.setBackground(Color.white);
+    }
+
+    private void initializeRightContainer() {
+        rightContainer = new JPanel();
+        rightContainer.setLayout(new BorderLayout());
+
+        rightDownSubContainer = new JPanel();
+        rightDownSubContainer.setLayout(new GridLayout());
+        rightDownSubContainer.add(tabbedPane);
+
+        rightContainer.add(labelAboutExample, BorderLayout.NORTH);
+        rightContainer.add(rightDownSubContainer, BorderLayout.CENTER);
+    }
+
+    private void initializeTabbedPane() {
+        preview = new JPanel();
+        preview.setLayout(new BorderLayout());
+
+        source = new JTextPane();
+        sourceContainer = new JScrollPane(source);
+
+        tabbedPane = new JTabbedPane();
+        tabbedPane.addTab("Preview", preview);
+        tabbedPane.addTab("Source", sourceContainer);
+        tabbedPane.setVisible(false);
+    }
+
+    private void initializeMainContainer() {
+        mainContainer = new JSplitPane();
+        mainContainer.setLeftComponent(leftContainer);
+        mainContainer.setRightComponent(rightContainer);
+    }
+
+    private void updatePreview() {
+        preview.removeAll();
+        preview.revalidate();
+    }
+
+    private void updateTabbedPane() {
+        tabbedPane.setVisible(true);
+        tabbedPane.setSelectedIndex(0);
     }
 
     public static void main(String[] args) {
