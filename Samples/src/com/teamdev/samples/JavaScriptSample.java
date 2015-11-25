c68b0cafd6e21ee8fad3a5d279727496bbe36ff7
Denis Malyshev <denis.malyshev@teamdev.com>
2015-11-25 17:27:41 +0200

    Implemented JavaScriptSample

---
+++ Samples/src/com/teamdev/samples/JavaScriptSample.java
@@ -0,0 +1,103 @@
+package com.teamdev.samples;
+
+
+import com.teamdev.demo.DemoSample;
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.JSValue;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class JavaScriptSample implements DemoSample {
+    private Browser browser;
+
+    @Override
+    public void run(JPanel container) {
+        browser = new Browser();
+        BrowserView browserView = new BrowserView(browser);
+        JSConsole jsConsole = new JSConsole();
+        JTextField addressBar = new JTextField("http://google.com");
+        addressBar.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == 10) {
+                    browser.loadURL(addressBar.getText());
+                    SwingUtilities.invokeLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            addressBar.setText(browser.getURL());
+                        }
+                    });
+                }
+            }
+        });
+        final JPanel topPanel = new JPanel();
+        topPanel.setLayout(new BorderLayout());
+        topPanel.add(addressBar, BorderLayout.NORTH);
+        topPanel.add(browserView, BorderLayout.CENTER);
+        final JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BorderLayout());
+        bottomPanel.add(jsConsole, BorderLayout.CENTER);
+        JSplitPane splitPane = new JSplitPane();
+        splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setTopComponent(topPanel);
+        splitPane.setBottomComponent(bottomPanel);
+        container.add(splitPane);
+        browser.loadURL(addressBar.getText());
+    }
+
+    @Override
+    public void disposeInstance() {
+        browser.dispose();
+    }
+
+    private class JSConsole extends JPanel {
+        private JTextField jsCodeArea;
+        private JTextArea console;
+        private JPanel consolePanel;
+
+        public JSConsole() {
+            initComponents();
+            setLayout(new BorderLayout());
+            add(new JScrollPane(consolePanel), BorderLayout.CENTER);
+            add(jsCodeArea, BorderLayout.SOUTH);
+        }
+
+        private void initComponents() {
+            initJSCodeArea();
+            initConsolePanel();
+        }
+
+        private void initConsolePanel() {
+            initConsole();
+            consolePanel = new JPanel();
+            consolePanel.setLayout(new BorderLayout());
+            JLabel title = new JLabel("JavaScript Console");
+            consolePanel.add(title, BorderLayout.NORTH);
+            consolePanel.add(console, BorderLayout.CENTER);
+        }
+
+        private void initConsole() {
+            console = new JTextArea();
+            console.setEditable(false);
+        }
+
+        private void initJSCodeArea() {
+            jsCodeArea = new JTextField();
+            jsCodeArea.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    if (e.getKeyCode() == 10) {
+                        final String script = jsCodeArea.getText();
+                        jsCodeArea.setText("");
+                        JSValue jsValue = browser.executeJavaScriptAndReturnValue(script);
+                        console.append(jsValue.toString() + "\n");
+                    }
+                }
+            });
+        }
+    }
+}
